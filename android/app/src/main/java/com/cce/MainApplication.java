0b5c7b10db1183c45fbab3de92e3e6d3e4ad55e8
josh <griffinjoshua5@gmail.com>
2020-07-20 11:05:07 +1200

    Add react-native-gesture-handler

--- android/app/src/main/java/com/cce/MainApplication.java
+++ android/app/src/main/java/com/cce/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
