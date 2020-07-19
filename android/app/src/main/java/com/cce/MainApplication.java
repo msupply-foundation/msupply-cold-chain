8b9f233b922568d9f8bb2380178281386a182ef4
josh <griffinjoshua5@gmail.com>
2020-07-20 11:14:54 +1200

    Add react-native-screens. Last native module.

--- android/app/src/main/java/com/cce/MainApplication.java
+++ android/app/src/main/java/com/cce/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.rnscreens.RNScreensPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
