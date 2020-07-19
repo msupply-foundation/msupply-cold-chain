ecfb0c4edea9fedac66e46edfaf179750c78d73b
josh <griffinjoshua5@gmail.com>
2020-07-20 11:06:19 +1200

    Add react-native-localize

--- android/app/src/main/java/com/cce/MainApplication.java
+++ android/app/src/main/java/com/cce/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
