fadd35b454cde0ec809889a0cf4ede8fa129eb48
josh <griffinjoshua5@gmail.com>
2020-07-20 11:10:36 +1200

    Add react-native safe-area-context

--- android/app/src/main/java/com/cce/MainApplication.java
+++ android/app/src/main/java/com/cce/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
