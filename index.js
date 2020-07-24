94fcb165384a950bfd929110fee072afebb98261
josh <griffinjoshua5@gmail.com>
2020-07-24 16:24:55 +1200

    Add a logbox ignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 
 import App from './src/ui/App';
 import { name as appName } from './app.json';
+
+LogBox.ignoreLogs(['Require cycle:']);
 
 AppRegistry.registerComponent(appName, () => App);
