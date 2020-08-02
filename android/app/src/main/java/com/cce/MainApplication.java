772c5cdfa29e80aacce2578dbe0a6e7a709fc7ea
josh <griffinjoshua5@gmail.com>
2020-08-02 22:03:16 +1200

    Add react-native-image-picker

--- android/app/src/main/java/com/cce/MainApplication.java
+++ android/app/src/main/java/com/cce/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
