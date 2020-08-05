e8e9b12c283b73737be9cf85a214114c81015898
josh <griffinjoshua5@gmail.com>
2020-08-05 20:55:33 +1200

    Add BluetoothService tests

--- src/services/bluetooth/BluetoothService.js
+++ src/services/bluetooth/BluetoothService.js
@@ -1,12 +1,18 @@
-import { NativeModules } from 'react-native';
+import { BleManager } from './BleManager';
+import { BLUETOOTH_SERVICE, BLUETOOTH_SERVICE_ERROR } from '~constants';
 
-const DEFAULT_MANAGER = NativeModules.BleManager;
-const DEFAULT_CONFIG = { manufacturerId: 307 };
+const DEFAULT_MANAGER = BleManager;
+const DEFAULT_CONFIG = { manufacturerId: BLUETOOTH_SERVICE.DEFAULT_MANUFACTURER_ID };
 
 export class BluetoothService {
   constructor(manager = DEFAULT_MANAGER, config = DEFAULT_CONFIG) {
+    const { manufacturerId } = config;
+    if (!manufacturerId || typeof manufacturerId !== 'number') {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.NO_MANUFACTURER_ID);
+    }
+
     this.manager = manager;
-    this.config = config;
+    this.config = { ...config };
   }
 
   get manufacturerId() {
@@ -14,6 +20,10 @@ export class BluetoothService {
   }
 
   scanForDevices = async () => {
+    if (!(this.manufacturerId && typeof this.manufacturerId === 'number')) {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.NO_MANUFACTURER_ID_SET);
+    }
+
     const { success, data, error } = await this.manager.getDevices(this.manufacturerId, '');
 
     if (success) return data;
@@ -21,6 +31,18 @@ export class BluetoothService {
   };
 
   sendCommand = async (macAddress, command) => {
+    if (!(macAddress && typeof macAddress === 'string')) {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.NO_MAC_ADDRESS);
+    }
+
+    if (!(command && typeof command === 'string')) {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.NO_COMMAND);
+    }
+
+    if (!(this.manufacturerId && typeof this.manufacturerId === 'number')) {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.NO_MANUFACTURER_ID_SET);
+    }
+
     return this.manager.sendCommand(this.manufacturerId, macAddress, command);
   };
 
@@ -28,7 +50,8 @@ export class BluetoothService {
     const { success, error } = await this.sendCommand(macAddress, '*blink');
 
     if (success) return success;
-    throw error;
+
+    throw new Error(`${error?.code} - ${error?.message}`);
   };
 
   downloadLogs = async macAddress => {
@@ -36,25 +59,41 @@ export class BluetoothService {
 
     const logs = data?.[0]?.logs;
     if (success && logs) return logs;
-    if (!success) throw error;
+    if (!success) throw new Error(`${error?.code} - ${error.message}`);
+
     throw new Error();
   };
 
-  setLogInterval = async (macAddress, interval = 300) => {
+  setLogInterval = async (macAddress, interval = BLUETOOTH_SERVICE.DEFAULT_INTERVAL) => {
+    const intervalWithinBounds =
+      interval > BLUETOOTH_SERVICE.MIN_INTERVAL && interval < BLUETOOTH_SERVICE.MAX_INTERVAL;
+    if (!(typeof interval === 'number' && intervalWithinBounds)) {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.INVALID_LOGGING_INTERVAL);
+    }
+
     const command = `*lint${interval}`;
     const result = await this.sendCommand(macAddress, command);
     const { success, error } = result;
 
     if (success) return success;
-    throw error;
+
+    throw new Error(`${error?.code} - ${error?.message}`);
   };
 
   setAdvertisementInterval = async (macAddress, interval = 300) => {
+    const intervalWithinBounds =
+      interval > BLUETOOTH_SERVICE.MIN_INTERVAL && interval < BLUETOOTH_SERVICE.MAX_INTERVAL;
+
+    if (!(typeof interval === 'number' && intervalWithinBounds)) {
+      throw new Error(BLUETOOTH_SERVICE_ERROR.INVALID_LOGGING_INTERVAL);
+    }
+
     const command = `*sadv${interval}`;
     const result = await this.sendCommand(macAddress, command);
     const { success, error } = result;
 
     if (success) return success;
-    throw error;
+
+    throw new Error(`${error?.code} - ${error?.message}`);
   };
 }
