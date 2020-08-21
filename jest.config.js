e9b6b4e1008153fbf3d3e79d1a2fd9058ba96d7e
josh <griffinjoshua5@gmail.com>
2020-08-21 23:43:53 +1200

    Add updates to jest config settings

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   preset: 'react-native',
-  setupFiles: ['<rootDir>/test-setup.js'],
-  transformIgnorePatterns: [],
+  setupFiles: [
+    '<rootDir>/test-setup.js',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|typeorm)',
+  ],
   testEnvironment: 'node',
   globals: {
     __DEV__: true,
