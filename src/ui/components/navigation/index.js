72ecfb8696d00e78ef5a3b6c272fbca1b1f2a825
josh <griffinjoshua5@gmail.com>
2020-08-22 00:06:35 +1200

    Add exporting of navigation components

---
+++ src/ui/components/navigation/index.js
@@ -0,0 +1,4 @@
+export { MainTabNavigator, MainTabScreen } from './MainTabNavigator';
+export { MainStackNavigator, MainStackScreen } from './MainStackNavigator';
+export { SettingsStackNavigator, SettingsStackScreen } from './SettingsStackNavigator';
+export { StackHeader } from './StackHeader';
