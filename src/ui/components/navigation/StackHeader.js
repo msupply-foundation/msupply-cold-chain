cc8a5bcd3d9f51b0bc560e67b23eaa951f1fda77
josh <griffinjoshua5@gmail.com>
2020-08-22 00:06:22 +1200

    Add StackHeader component

---
+++ src/ui/components/navigation/StackHeader.js
@@ -0,0 +1,26 @@
+import { Pressable } from 'react-native';
+
+import { COLOUR } from '~constants';
+import { t } from '~translations';
+
+import { Centered, SettingsRowLayout } from '~layouts';
+import { Chevron } from '~presentation/icons';
+import { LargeText } from '~presentation/typography';
+
+const LeftIcon = ({ goBack }) => (
+  <Pressable hitSlop={100} onPress={goBack}>
+    <Centered style={{ marginTop: 10 }}>
+      <Chevron direction="left" colour={COLOUR.GREY_ONE} />
+    </Centered>
+  </Pressable>
+);
+
+export const StackHeader = ({ name, goBack, RightComponent }) => {
+  return (
+    <SettingsRowLayout
+      LeftIcon={goBack ? <LeftIcon goBack={goBack} /> : null}
+      Text={<LargeText colour={COLOUR.SECONDARY}>{t(name)}</LargeText>}
+      RightComponent={RightComponent}
+    />
+  );
+};
