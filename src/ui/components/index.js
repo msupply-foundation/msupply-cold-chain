2775e175a11d0726d9a17ba7cff00efb083260c8
josh <griffinjoshua5@gmail.com>
2020-08-26 00:26:41 +1200

    Add export

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,3 +1,4 @@
 export { SensorStatus } from './SensorStatus';
 export { SensorChartRow } from './SensorChartRow';
 export { PinEntry } from './PinEntry';
+export { UpdatingSensorModal } from './UpdatingSensorModal';
