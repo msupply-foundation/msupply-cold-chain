fbe10765d2dd6110b77e346113254c0588e20c3e
josh <griffinjoshua5@gmail.com>
2020-08-26 00:01:43 +1200

    Add updated RootReducer and RootSaga with Sensor, Setting and BreachConfiguration

--- src/shared/store/RootReducer.js
+++ src/shared/store/RootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import { BluetoothReducer } from '~bluetooth/bluetoothSlice';
 import { DatabaseReducer } from '~database';
+import { SensorReducer } from '~sensor';
+import { SettingReducer } from '~setting';
+import { BreachConfigurationReducer } from '~breachConfiguration';
 
 export const RootReducer = combineReducers({
   bluetooth: BluetoothReducer,
   database: DatabaseReducer,
+  sensor: SensorReducer,
+  setting: SettingReducer,
+  breachConfiguration: BreachConfigurationReducer,
 });
--- src/shared/store/RootSaga.js
+++ src/shared/store/RootSaga.js
@@ -1,15 +1,20 @@
 import createSagaMiddleware from 'redux-saga';
 import { fork } from 'redux-saga/effects';
-
+import { SensorSaga } from '~sensor';
+import { BreachConfigurationSaga } from '~breachConfiguration';
+import { SettingSaga } from '~setting';
 import { WatchDatabaseActions } from '~database';
 import { BluetoothServiceWatcher } from '~bluetooth';
-import { getService } from '~services';
+import { getService, getServices } from '~services';
 
 export function* RootSaga() {
   yield fork(BluetoothServiceWatcher);
   yield fork(WatchDatabaseActions);
+  yield fork(SensorSaga.watchSensorActions);
+  yield fork(SettingSaga.WatchSettingActions);
+  yield fork(BreachConfigurationSaga.watchBreachConfigurationActions);
 }
 
 export const SagaMiddleware = createSagaMiddleware({
-  context: { getService },
+  context: { getService, getServices },
 });
