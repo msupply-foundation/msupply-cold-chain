5d43e4c4c1701628bafe86485ae8d42ff7460d97
josh <griffinjoshua5@gmail.com>
2020-08-26 00:40:24 +1200

    Add App/DependencyContainer initial implementation

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,103 +1,161 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from 'react';
+import { getStorybookUI, configure } from '@storybook/react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { NavigationContainer, useIsFocused } from '@react-navigation/native';
+import { View } from 'react-native';
+import moment from 'moment';
 
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
+import { NAVIGATION } from '~constants';
+
+import { PinEntry, SensorChartRow } from '~components';
+import { Gradient } from '~layouts';
+
+import { Button } from '~components/buttons';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  SettingsStackNavigator,
+  SettingsStackScreen,
+  MainStackNavigator,
+  MainStackScreen,
+  MainTabNavigator,
+  MainTabScreen,
+} from './components/navigation';
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+import {
+  MainSettingsScreen,
+  BluetoothSettingsScreen,
+  TemperatureBreachSettingsScreen,
+  SensorSettingsScreen,
+  SensorDetailScreen,
+  TemperatureBreachDetailScreen,
+  DevSettingsScreen,
+} from '~screens/settings';
+
+import { StoreRehydrateContainer } from './StoreRehydrateContainer';
+import { DependencyContainer } from './DependencyContainer';
+import { ReduxContainer } from './ReduxContainer';
+
+configure(() => {
+  require('./storybook/stories');
+}, module);
+
+// eslint-disable-next-line no-unused-vars
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: require('@react-native-community/async-storage').default,
 });
+
+const getData = n => {
+  const date = moment().subtract(30 * n, 'minutes');
+
+  const getTemperature = () => Number((Math.random() * 10).toFixed(2));
+
+  return Array.from({ length: n }).map((_, i) => ({
+    timestamp: moment(date)
+      .add(30 * i, 'hour')
+      .toDate(),
+    temperature: getTemperature(),
+  }));
+};
+
+const ScreenOne = ({ navigation }) => {
+  const logs = getData(30);
+
+  return (
+    <LinearGradient colors={['#282F46', '#4C68A3']} style={{ height: '100%' }}>
+      <Button
+        title="Go to sensor"
+        style={{ backgroundColour: 'red' }}
+        onPress={() => navigation.navigate('Sensor')}
+      />
+
+      <SensorChartRow logs={logs} />
+    </LinearGradient>
+  );
+};
+
+const Mapz = () => {
+  return <View style={{}} />;
+};
+
+const sensor = { name: 'sensor one', temperature: '3.2c' };
+
+const Sensor = () => {
+  return (
+    <LinearGradient colors={['#282F46', '#4C68A3']} style={{ height: '100%' }}>
+      <SensorChartRow sensor={sensor} logs={getData(30)} direction="left" />
+    </LinearGradient>
+  );
+};
+
+const Settings = ({ navigation }) => {
+  const [authorized] = useState(false);
+
+  const onComplete = () => {};
+
+  const isFocused = useIsFocused();
+
+  return !authorized && !__DEV__ ? (
+    isFocused && <PinEntry onComplete={onComplete} onCancel={() => navigation.goBack()} />
+  ) : (
+    <SettingsStackNavigator>
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.MENU}
+        component={MainSettingsScreen}
+      />
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.BLUETOOTH}
+        component={BluetoothSettingsScreen}
+      />
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.SENSORS}
+        component={SensorSettingsScreen}
+      />
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH}
+        component={TemperatureBreachSettingsScreen}
+      />
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.SENSOR_DETAIL}
+        component={SensorDetailScreen}
+      />
+
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH_DETAIL}
+        component={TemperatureBreachDetailScreen}
+      />
+
+      <SettingsStackScreen
+        name={NAVIGATION.SCREENS.SETTINGS_STACK.DEVELOPER}
+        component={DevSettingsScreen}
+      />
+    </SettingsStackNavigator>
+  );
+};
+
+const Tabs = () => (
+  <MainTabNavigator>
+    <MainTabScreen name="Sensors" component={ScreenOne} />
+    <MainTabScreen name="Mapz" component={Mapz} />
+    <MainTabScreen name="Settings" component={Settings} />
+  </MainTabNavigator>
+);
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then
-                come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <ReduxContainer>
+      <DependencyContainer>
+        <StoreRehydrateContainer>
+          <NavigationContainer>
+            <Gradient>
+              <MainStackNavigator>
+                <MainStackScreen name="Tabs" component={Tabs} />
+                <MainStackScreen name="Sensor" component={Sensor} />
+              </MainStackNavigator>
+            </Gradient>
+          </NavigationContainer>
+        </StoreRehydrateContainer>
+      </DependencyContainer>
+    </ReduxContainer>
   );
 };
 
--- src/ui/DependencyContainer.js
+++ src/ui/DependencyContainer.js
@@ -1,14 +1,25 @@
-import { useEffect, createContext } from 'react';
+import { useEffect, createContext, useState } from 'react';
+import { BleManager } from 'react-native-ble-plx';
+
 import { Database, DatabaseService } from '~database';
-import { BluetoothService } from '~bluetooth';
-import { DeviceService } from '~services/device/DeviceService';
+import { SERVICES } from '~constants';
 import { registerService, getService } from '~services';
 
-import { SERVICES } from '~constants';
+import { DeviceService } from '~services/device/DeviceService';
+import { SensorManager } from '~sensor';
+
+import { DatabaseUtilsService } from '../services/database/DatabaseUtilsService';
+import { Ble } from '../services/bluetooth/Ble';
+import { SettingManager } from '../features/setting/SettingManager';
+import { BreachConfigurationManager } from '../features/breachConfiguration';
 
 const ServiceLocatorContext = createContext(null);
 
+const bleManager = new BleManager();
+
 export const DependencyContainer = props => {
+  const [ready, setReady] = useState(false);
+
   // When mounting, set up the app dependencies. This is ...
   // ... where all dependencies should be instantiated   ...
   // ... and imported. If you require some dependency    ...
@@ -17,16 +28,39 @@ export const DependencyContainer = props => {
   useEffect(() => {
     const db = new Database();
     const dbService = new DatabaseService(db);
-    const btService = new BluetoothService();
     const deviceService = new DeviceService();
+    const dbUtilsService = new DatabaseUtilsService();
+    const settingManager = new SettingManager(dbService);
+    const breachConfigurationManager = new BreachConfigurationManager(dbService);
+
+    (async () => {
+      await db.getConnection();
+      setReady(true);
+    })();
+    const sensorsManager = new SensorManager(dbService);
+
+    // let btService = new BluetoothService();
+    // if (__DEV__) {
+    //   btService = new DevBluetoothService();
+    // }
+
+    const btService = new Ble(bleManager);
+
+    registerService(SERVICES.BREACH_CONFIGURATION_MANAGER, breachConfigurationManager);
+    registerService(SERVICES.SENSOR_MANAGER, sensorsManager);
+    registerService(SERVICES.DATABASE_UTILS, dbUtilsService);
     registerService(SERVICES.DEVICE, deviceService);
     registerService(SERVICES.BLUETOOTH, btService);
     registerService(SERVICES.DATABASE, dbService);
+
+    registerService(SERVICES.SETTING_MANAGER, settingManager);
   }, []);
 
   const { children } = props;
 
   return (
-    <ServiceLocatorContext.Provider value={getService}>{children}</ServiceLocatorContext.Provider>
+    <ServiceLocatorContext.Provider value={getService}>
+      {ready ? children : null}
+    </ServiceLocatorContext.Provider>
   );
 };
