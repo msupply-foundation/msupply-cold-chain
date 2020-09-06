3a0dad0494eacfbeae8cbed4804b6e6afb92f018
josh <griffinjoshua5@gmail.com>
2020-09-06 21:21:01 +1200

    Add exports

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -2,3 +2,5 @@ export { SensorStatus } from './SensorStatus';
 export { SensorChartRow } from './SensorChartRow';
 export { PinEntry } from './PinEntry';
 export { UpdatingSensorModal } from './UpdatingSensorModal';
+export { BatteryStatus } from './BatteryStatus';
+export { DateRangePicker } from './DateRangePicker';
