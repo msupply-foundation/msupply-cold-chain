b9db80e7633d392e032f731b984cfe5499ba17d7
josh <griffinjoshua5@gmail.com>
2020-09-06 20:33:12 +1200

    Add TemperatureLog feature

--- src/features/temperatureLog/TemperatureLogSlice.js
+++ src/features/temperatureLog/TemperatureLogSlice.js
@@ -2,20 +2,33 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { REDUCER_SHAPE } from '~constants';
 
-const initialState = {};
-const reducers = {};
+const initialState = {
+  logsBySensor: {},
+};
+
+const reducers = {
+  hydrate: () => {},
+  hydrateSuccessful: {
+    prepare: logsBySensor => ({ payload: { logsBySensor } }),
+    reducer: (draftState, { payload: { logsBySensor } }) => {
+      draftState.logsBySensor = logsBySensor;
+    },
+  },
+  hydrateFailed: () => {},
+};
+
 const { actions: TemperatureLogAction, reducer: TemperatureLogReducer } = createSlice({
   name: REDUCER_SHAPE.TEMPERATURE_LOG,
   initialState,
   reducers,
 });
 
-const TemperatureLogSelector = {};
+const TemperatureLogSelector = {
+  getAll: ({ temperatureLog: { logsBySensor } }) => {
+    return logsBySensor;
+  },
+};
 
-function* watchTemperatureLogActions() {
-  yield;
-}
-
-const TemperatureLogSaga = { watchTemperatureLogActions };
+const TemperatureLogSaga = {};
 
 export { TemperatureLogAction, TemperatureLogReducer, TemperatureLogSaga, TemperatureLogSelector };
