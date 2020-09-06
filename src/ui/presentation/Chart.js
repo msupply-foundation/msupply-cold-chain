e726f844e0d4c85adaff076e9b93f78e9487a30a
josh <griffinjoshua5@gmail.com>
2020-09-06 21:38:25 +1200

    Add updated Chart

--- src/ui/presentation/Chart.js
+++ src/ui/presentation/Chart.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { VictoryLine, VictoryChart, VictoryAxis } from 'victory-native';
 
 import { FONT, STYLE, COLOUR, CHART } from '~constants';
-import { tickFormat } from '~formatters';
+import { getTickFormatter } from '~formatters';
 
 import { ChartGradient } from './ChartGradient';
+import { Centered } from '../layouts';
 
 const style = {
-  container: { height: STYLE.HEIGHT.NORMAL_CHART, width: STYLE.WIDTH.NORMAL_CHART },
+  container: {
+    height: STYLE.HEIGHT.NORMAL_CHART,
+    width: STYLE.WIDTH.NORMAL_CHART,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   yAxis: {
     tickLabels: {
       stroke: COLOUR.PRIMARY,
@@ -29,34 +35,58 @@ const style = {
   line: { data: { stroke: CHART.LINE_GRADIENT_ID } },
 };
 
-export const Chart = ({ data }) => {
+const round5 = n => {
+  return Math.ceil(n / 5) * 5;
+};
+
+export const Chart = ({
+  data,
+  width = STYLE.WIDTH.NORMAL_CHART,
+  height = STYLE.HEIGHT.NORMAL_CHART,
+}) => {
+  const minTemp = Math.min(...data.map(({ temperature }) => temperature));
+  const maxTemp = Math.max(...data.map(({ temperature }) => temperature));
+
+  const domainMin = round5(minTemp) - 5;
+  const domainMax = round5(maxTemp) + 5;
+
+  const minTime = Math.min(...data.map(({ timestamp }) => timestamp));
+  const maxTime = Math.max(...data.map(({ timestamp }) => timestamp));
+
   return (
-    <View style={style.container}>
-      <VictoryChart
-        height={STYLE.HEIGHT.NORMAL_CHART}
-        width={STYLE.WIDTH.NORMAL_CHART}
-        domainPadding={CHART.DOMAIN_PADDING}
-      >
-        {/* Y AXIS */}
-        <VictoryAxis
-          crossAxis
-          numberOfTicks={CHART.NUMBER_OF_TICKS_Y}
-          dependentAxis
-          style={style.yAxis}
-        />
+    <View style={[style.container, { height, width }]}>
+      {data.length ? (
+        <VictoryChart width={width} domain={{ y: [domainMin, domainMax], x: [minTime, maxTime] }}>
+          {/* X AXIS */}
+          <VictoryAxis
+            orientation="bottom"
+            style={style.xAxis}
+            tickFormat={getTickFormatter()}
+            tickCount={5}
+          />
 
-        {/* X AXIS */}
-        <VictoryAxis crossAxis tickFormat={tickFormat} style={style.xAxis} />
+          {/* Y AXIS */}
+          <VictoryAxis
+            dependentAxis
+            orientation="left"
+            numberOfTicks={CHART.NUMBER_OF_TICKS_Y}
+            style={style.yAxis}
+          />
 
-        <ChartGradient />
-        <VictoryLine
-          data={data}
-          x={CHART.X_KEY}
-          y={CHART.Y_KEY}
-          interpolation={CHART.INTERPOLATION}
-          style={style.line}
-        />
-      </VictoryChart>
+          <ChartGradient />
+          <VictoryLine
+            data={data}
+            x={CHART.X_KEY}
+            y={CHART.Y_KEY}
+            interpolation={CHART.INTERPOLATION}
+            style={style.line}
+          />
+        </VictoryChart>
+      ) : (
+        <Centered style={{ width: '100%' }}>
+          <ActivityIndicator size="large" color={COLOUR.PRIMARY} />
+        </Centered>
+      )}
     </View>
   );
 };
