90ef3a9fb6e4557ec2ef2a12247fcb21024e7899
josh <griffinjoshua5@gmail.com>
2020-09-06 20:01:40 +1200

    Add updated SensorSlice/Manager

--- src/features/sensor/SensorManager.js
+++ src/features/sensor/SensorManager.js
@@ -1,5 +1,77 @@
+import moment from 'moment';
 import { ENTITIES } from '~constants';
 import { uuid } from '~services/utilities';
+
+const SENSOR_STATE = `
+SELECT s.id id,
+s.macAddress macAddress,
+s.logInterval logInterval,
+s.name        name,
+s.logDelay    logDelay,
+mostRecentLogTimestamp,
+firstTimestamp,
+coalesce(numberOfLogs,0) numberOfLogs,
+currentTemperature,
+CASE when mostRecentLogTimestamp - firstTimestamp > (3 * 24 * 60 * 60) then mostRecentLogTimestamp - 24 * 3 * 60 * 60 else firstTimestamp end as minChartTimestamp,
+CASE
+WHEN endTimestamp IS NULL AND temperatureBreachConfigurationId = 'HOT_BREACH' THEN 1 ELSE 0 END AS isInHotBreach,
+CASE WHEN endTimestamp IS NULL AND temperatureBreachConfigurationId = 'COLD_BREACH' THEN 1 ELSE 0 END AS isInColdBreach
+FROM      sensor s OUTER
+
+left JOIN
+(
+  SELECT  max(timestamp) mostRecentLogTimestamp,
+          min(timestamp) firstTimestamp,
+          temperature    currentTemperature,
+          count(*) numberOfLogs,
+          tl.sensorid,
+          *
+  FROM temperaturelog tl 
+  LEFT OUTER JOIN
+    (
+      SELECT   max(startTimestamp) startTimestamp,
+      temperatureBreachConfigurationId,
+      endTimestamp,
+      sensorid
+      FROM temperaturebreach tb
+      GROUP BY sensorid
+
+    ) tb
+  ON tl.sensorid = tb.sensorid
+  GROUP BY tl.sensorid
+  order by timestamp
+) logs
+
+  ON logs.sensorid = s.id 
+where s.id = ?
+`;
+
+// If using sensor logs, will need to use this one.
+// const MOST_RECENT_TIMESTAMP = `
+// select
+//   coalesce(case when sensorLogMaxTimestamp > temperatureLogMaxTimestamp
+//     then sensorLogMaxTimestamp
+//     else temperatureLogMaxTimestamp
+//   end, 0) as mostRecentTimestamp
+// from (
+//   select
+//   (select max(timestamp) from temperaturelog where sensorid = ?) temperatureLogMaxTimestamp,
+//   (select max(timestamp) from sensorlog where sensorid = ?) sensorLogMaxTimestamp
+// )
+// `;
+
+const MOST_RECENT_TIMESTAMP = `
+select coalesce(max(timestamp), 0) mostRecentTimestamp
+from temperaturelog
+where sensorid = ?
+`;
+
+const CAN_DOWNLOAD = `
+select coalesce(s.logDelay,0) logDelay, s.logInterval + coalesce(max(tl.timestamp),0) nextPossibleLogTime
+from sensor s
+left outer join temperaturelog tl on s.id = tl.sensorid
+where s.id = ?
+`;
 
 export class SensorManager {
   constructor(dbService) {
@@ -10,8 +82,108 @@ export class SensorManager {
     return this.databaseService.upsert(ENTITIES.SENSOR, ...params);
   };
 
-  getSensors = async () => {
+  getAll = async () => {
     return this.databaseService.getAll(ENTITIES.SENSOR);
+  };
+
+  getSensor = async macAddress => {
+    const sensor = await this.databaseService.queryWith(ENTITIES.SENSOR, { macAddress });
+
+    return sensor[0];
+  };
+
+  getMostRecentLogTime = async id => {
+    const manager = await this.databaseService.getEntityManager();
+    const result = await manager.query(MOST_RECENT_TIMESTAMP, [id]);
+
+    const { mostRecentTimestamp = 0 } = result[0] ?? {};
+    return mostRecentTimestamp;
+  };
+
+  getLogInterval = async id => {
+    const sensor = await this.getSensorById(id);
+    const { logInterval } = sensor ?? {};
+    return logInterval;
+  };
+
+  getCanDownload = async id => {
+    const manager = await this.databaseService.getEntityManager();
+    const result = await manager.query(CAN_DOWNLOAD, [id]);
+    const { logDelay, nextPossibleLogTime } = result[0];
+    const now = moment().unix();
+
+    const canDownloadLogs = now >= logDelay && nextPossibleLogTime <= now;
+
+    return [canDownloadLogs];
+  };
+
+  getSensorById = async id => {
+    const sensor = await this.databaseService.queryWith(ENTITIES.SENSOR, { id });
+    return sensor[0];
+  };
+
+  updateLastDownloadTime = async sensor => {
+    const { id, logInterval } = sensor;
+    const lastDownloadTime = moment().unix();
+    const nextDownloadTime = moment().add(logInterval, 's').unix();
+
+    return this.upsert({ id, lastDownloadTime, nextDownloadTime });
+  };
+
+  getSensorState = async sensorId => {
+    const manager = await this.databaseService.getEntityManager();
+    const result = await manager.query(SENSOR_STATE, [sensorId]);
+    const resultTwo = await manager.query(
+      'select s.batteryLevel batteryLevel, temperature as currentTemperature from sensor s left join temperaturelog tl on tl.sensorid = s.id where s.id = ? order by timestamp desc limit 1',
+      [sensorId]
+    );
+
+    const { batteryLevel, currentTemperature = 'N/A' } = resultTwo[0] ?? {};
+
+    return { ...result[0], currentTemperature, batteryLevel };
+  };
+
+  getSensors = async () => {
+    const manager = await this.databaseService.getEntityManager();
+
+    return manager.query(
+      `
+      SELECT s.id id,
+      s.macAddress macAddress,
+      s.logInterval logInterval,
+      s.name        name,
+      s.logDelay    logDelay,
+      mostRecentLogTimestamp,
+      firstTimestamp,
+      currentTemperature,
+      CASE when mostRecentLogTimestamp - firstTimestamp > (3 * 24 * 60 * 60) then mostRecentLogTimestamp - 24 * 3 * 60 * 60 else firstTimestamp end as minChartTimestamp,
+      CASE
+      WHEN endTimestamp IS NULL AND temperatureBreachConfigurationId = 'HOT_BREACH' THEN 1 ELSE 0 END AS isInHotBreach,
+      CASE WHEN endTimestamp IS NULL AND temperatureBreachConfigurationId = 'COLD_BREACH' THEN 1 ELSE 0 END AS isInColdBreach
+      FROM      sensor s OUTER
+      left JOIN
+      (
+        SELECT  max(timestamp) mostRecentLogTimestamp,
+                min(timestamp) firstTimestamp,
+                temperature    currentTemperature,
+                tl.sensorid,
+                *
+        FROM temperaturelog tl
+        LEFT OUTER JOIN
+          (
+            SELECT   max(startTimestamp) startTimestamp,
+            temperatureBreachConfigurationId,
+            endTimestamp,
+            sensorid
+            FROM temperaturebreach tb
+            GROUP BY sensorid
+          ) tb
+        ON tl.sensorid = tb.sensorid
+        GROUP BY tl.sensorid
+      ) logs
+        ON logs.sensorid = s.id 
+      `
+    );
   };
 
   getTemperatureBreachConfigs = async () => {
@@ -30,9 +202,9 @@ export class SensorManager {
     return joinRecord;
   };
 
-  addNewSensor = async (macAddress, logInterval) => {
+  addNewSensor = async (macAddress, logInterval, logDelay, batteryLevel) => {
     const id = uuid();
-    return this.upsert({ logInterval, macAddress, id });
+    return this.upsert({ logInterval, macAddress, id, batteryLevel, logDelay });
   };
 
   updateField = async (id, key, value) => {
@@ -49,5 +221,9 @@ export class SensorManager {
 
   updateLogInterval = async (id, logInterval) => {
     return this.upsert({ id, logInterval });
+  };
+
+  updateBatteryLevel = async (id, batteryLevel) => {
+    return this.upsert({ id, batteryLevel });
   };
 }
--- src/features/sensor/SensorSlice.js
+++ src/features/sensor/SensorSlice.js
@@ -1,15 +1,69 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { getContext, call, put, takeEvery } from 'redux-saga/effects';
 
+import { createSelector } from 'reselect';
 import { SERVICES } from '~constants';
+import { HydrateAction } from '../hydrate/hydrateSlice';
 
 const initialState = {
   byId: {},
   ids: [],
   foundSensors: [],
+  status: {},
 };
 
 const reducers = {
+  updateBatteryLevel: {
+    prepare: (sensorId, newBatteryLevel) => ({
+      payload: { batteryLevel: newBatteryLevel, sensorId },
+    }),
+    reducer: () => {},
+  },
+  updateBatteryLevelSuccessful: {
+    prepare: (sensorId, batteryLevel) => ({ payload: { sensorId, batteryLevel } }),
+    reducer: (draftState, { payload: { batteryLevel, sensorId } }) => {
+      draftState.status[sensorId].batteryLevel = batteryLevel;
+    },
+  },
+  updateBatteryLevelFailed: () => {},
+
+  updateLogDelay: {
+    prepare: (sensorId, newLogDelay) => ({ payload: { logDelay: newLogDelay, sensorId } }),
+    reducer: () => {},
+  },
+  updateLogDelaySuccessful: () => {},
+  updateLogDelayFailed: () => {},
+
+  toggleDownloading: {
+    prepare: (sensorId, isDownloading) => ({ payload: { isDownloading, sensorId } }),
+    reducer: (draftState, { payload: { isDownloading, sensorId } }) => {
+      draftState.status[sensorId].isDownloading = isDownloading;
+    },
+  },
+
+  getSensor: { prepare: () => {}, reducer: () => {} },
+  getSensorSuccessful: {
+    prepare: () => {},
+    reducer: (draftState, { payload: { sensor } }) => {
+      const { id } = sensor;
+      draftState.byId[id] = sensor;
+
+      if (!draftState.ids.includes(id)) {
+        draftState.ids.push(id);
+      }
+    },
+  },
+  getSensorFailed: () => {},
+
+  getSensorState: { prepare: sensorId => ({ payload: { sensorId } }), reducer: () => {} },
+  getSensorStateSuccessful: {
+    prepare: (sensorId, state) => ({ payload: { sensorId, state } }),
+    reducer: (draftState, { payload: { sensorId, state } }) => {
+      draftState.status[sensorId] = state;
+    },
+  },
+  getSensorStateFailed: () => {},
+
   hydrate: () => {},
   hydrateSuccessful: {
     reducer: (draftState, { payload: { sensors } }) => {
@@ -37,7 +91,9 @@ const reducers = {
 
   addNewSensor: {
     reducer: () => {},
-    prepare: (macAddress, logInterval) => ({ payload: { macAddress, logInterval } }),
+    prepare: (macAddress, logInterval, logDelay, batteryLevel) => ({
+      payload: { macAddress, logInterval, logDelay, batteryLevel },
+    }),
   },
   addNewSensorSuccessful: {
     reducer: (draftState, { payload: { sensor } }) => {
@@ -57,7 +113,9 @@ const reducers = {
         const alreadyFound = !!Object.values(draftState.byId).find(
           ({ macAddress: existingMac }) => existingMac === macAddress
         );
-        if (!alreadyFound) draftState.foundSensors.push(macAddress);
+        if (!alreadyFound) {
+          draftState.foundSensors.push(macAddress);
+        }
       }
     },
     prepare: macAddress => ({ payload: { macAddress } }),
@@ -79,6 +137,7 @@ function* hydrate() {
   try {
     const sensors = yield call(sensorManager.getSensors);
     yield put(SensorAction.hydrateSuccessful(sensors));
+    yield put(HydrateAction.sensor());
   } catch (error) {
     yield put(SensorAction.hydrateFailed(error));
   }
@@ -95,14 +154,62 @@ function* update({ payload: { id, key, value } }) {
   }
 }
 
-function* addNewSensor({ payload: { macAddress, logInterval } }) {
+function* addNewSensor({ payload: { macAddress, logInterval, logDelay, batteryLevel } }) {
   const getService = yield getContext('getService');
   const sensorManager = yield call(getService, SERVICES.SENSOR_MANAGER);
   try {
-    const newlyAddedSensor = yield call(sensorManager.addNewSensor, macAddress, logInterval);
+    const newlyAddedSensor = yield call(
+      sensorManager.addNewSensor,
+      macAddress,
+      logInterval,
+      logDelay,
+      batteryLevel
+    );
+
     yield put(SensorAction.addNewSensorSuccessful(newlyAddedSensor));
   } catch (error) {
     yield put(SensorAction.addNewSensorFailed(error));
+  }
+}
+
+function* getSensorState({ payload: { sensorId } }) {
+  const getService = yield getContext('getService');
+  const sensorManager = yield call(getService, SERVICES.SENSOR_MANAGER);
+  try {
+    const state = yield call(sensorManager.getSensorState, sensorId);
+    yield put(SensorAction.getSensorStateSuccessful(sensorId, state));
+  } catch (error) {
+    yield put(SensorAction.getSensorStateFailed(error));
+  }
+}
+
+function* updateLogDelay({ payload: { logDelay, sensorId } }) {
+  const getService = yield getContext('getService');
+  const sensorManager = yield call(getService, SERVICES.SENSOR_MANAGER);
+
+  try {
+    yield call(sensorManager.updateLogDelay, sensorId, logDelay);
+    yield put(SensorAction.updateLogDelaySuccessful);
+    yield put(SensorAction.getSensor(sensorId));
+  } catch (error) {
+    console.log('-------------------------------------------');
+    console.log('error', error.message);
+    console.log('-------------------------------------------');
+  }
+}
+
+function* updateBatteryLevel({ payload: { sensorId, batteryLevel } }) {
+  const getService = yield getContext('getService');
+  const sensorManager = yield call(getService, SERVICES.SENSOR_MANAGER);
+
+  try {
+    yield call(sensorManager.updateBatteryLevel, sensorId, batteryLevel);
+    yield put(SensorAction.updateBatteryLevelSuccessful(sensorId, batteryLevel));
+    yield put(SensorAction.getSensorState(sensorId));
+  } catch (error) {
+    console.log('-------------------------------------------');
+    console.log('error', error.message);
+    console.log('-------------------------------------------');
   }
 }
 
@@ -110,12 +217,28 @@ function* watchSensorActions() {
   yield takeEvery(SensorAction.hydrate, hydrate);
   yield takeEvery(SensorAction.addNewSensor, addNewSensor);
   yield takeEvery(SensorAction.update, update);
+  yield takeEvery(SensorAction.getSensorState, getSensorState);
+  yield takeEvery(SensorAction.updateLogDelay, updateLogDelay);
+  yield takeEvery(SensorAction.updateBatteryLevel, updateBatteryLevel);
 }
 
+const selectById = ({ sensor: { byId } }) => {
+  return byId;
+};
+
+const selectIds = ({ sensor: { ids } }) => {
+  return ids;
+};
+
 const SensorSelector = {
-  availableSensorsList: ({ sensor }) => {
-    const { byId, ids } = sensor;
+  availableSensorsList: createSelector([selectById, selectIds], (byId, ids) => {
     return ids.map(id => ({ id, name: byId[id].name ?? byId[id].macAddress }));
+  }),
+  macAddress: ({ sensor }, { id }) => {
+    return sensor.byId[id].macAddress;
+  },
+  macAddresses: ({ sensor: { byId, ids } }) => {
+    return ids.map(id => byId[id].macAddress);
   },
   foundSensorsList: ({ sensor }) => {
     const { foundSensors } = sensor;
@@ -128,6 +251,9 @@ const SensorSelector = {
   sensorsList: ({ sensor }) => {
     const { byId, ids } = sensor;
     return ids.map(id => byId[id]);
+  },
+  sensorIds: ({ sensor: { ids } }) => {
+    return ids;
   },
 };
 
