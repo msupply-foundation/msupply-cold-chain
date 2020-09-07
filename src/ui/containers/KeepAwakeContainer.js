033808a2cede72884ad48059d293f5eff76342e5
josh <griffinjoshua5@gmail.com>
2020-09-07 15:15:44 +1200

    Add extract KeepAwake to own component

---
+++ src/ui/containers/KeepAwakeContainer.js
@@ -0,0 +1,10 @@
+import { useKeepAwake } from 'expo-keep-awake';
+
+/**
+ * As long as this container is mounted, the app will stay 'awake', and
+ * not be sent to the background (unless explicitly sent by the user).
+ */
+export const KeepAwakeContainer = ({ children }) => {
+  useKeepAwake();
+  return children;
+};
