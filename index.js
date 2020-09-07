7364507119532bad8facf558698941be026e4c38
josh <griffinjoshua5@gmail.com>
2020-09-07 14:43:01 +1200

    Add updated twix import location

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { AppRegistry, LogBox } from 'react-native';
 
 import App from './src/ui/App';
 
+// Moment.js plugin for date ranges- importing at the root so it
+// injects moment.
+import 'twix';
+
 import { name as appName } from './app.json';
 
 if (__DEV__) {
