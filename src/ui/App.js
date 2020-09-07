6e5372df24cd0d320453fe5f645a9a3447c4dd63
josh <griffinjoshua5@gmail.com>
2020-09-07 14:52:24 +1200

    Add tidy up of App.js

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,16 +1,10 @@
-import React, { useState } from 'react';
-import { getStorybookUI, configure } from '@storybook/react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import { NavigationContainer, useIsFocused } from '@react-navigation/native';
-import { View } from 'react-native';
-import moment from 'moment';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 
 import { NAVIGATION } from '~constants';
-
-import { PinEntry, SensorChartRow } from '~components';
-import { Gradient } from '~layouts';
-
-import { Button } from '~components/buttons';
+import { MainHeader } from './components';
 
 import {
   SettingsStackNavigator,
@@ -26,119 +20,66 @@ import {
   BluetoothSettingsScreen,
   TemperatureBreachSettingsScreen,
   SensorSettingsScreen,
-  SensorDetailScreen,
   TemperatureBreachDetailScreen,
   DevSettingsScreen,
+  SensorDetailScreen as SensorSettingsDetailScreen,
+  CumulativeDetailSettingScreen,
 } from '~screens/settings';
 
-import { StoreRehydrateContainer } from './StoreRehydrateContainer';
-import { DependencyContainer } from './DependencyContainer';
-import { ReduxContainer } from './ReduxContainer';
+import { StoreRehydrateContainer } from './containers/StoreRehydrateContainer';
+import { DependencyContainer } from './containers/DependencyContainer';
+import { ReduxContainer } from './containers/ReduxContainer';
 
-configure(() => {
-  require('./storybook/stories');
-}, module);
+import { SensorListScreen, SensorDetailScreen } from './screens/sensor';
 
-// eslint-disable-next-line no-unused-vars
-const StorybookUIRoot = getStorybookUI({
-  asyncStorage: require('@react-native-community/async-storage').default,
-});
+const Settings = () => (
+  <SettingsStackNavigator>
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.MENU}
+      component={MainSettingsScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.BLUETOOTH}
+      component={BluetoothSettingsScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.SENSORS}
+      component={SensorSettingsScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH}
+      component={TemperatureBreachSettingsScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.SENSOR_DETAIL}
+      component={SensorSettingsDetailScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH_DETAIL}
+      component={TemperatureBreachDetailScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.DEVELOPER}
+      component={DevSettingsScreen}
+    />
+    <SettingsStackScreen
+      name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_CUMULATIVE_DETAIL}
+      component={CumulativeDetailSettingScreen}
+    />
+  </SettingsStackNavigator>
+);
 
-const getData = n => {
-  const date = moment().subtract(30 * n, 'minutes');
-
-  const getTemperature = () => Number((Math.random() * 10).toFixed(2));
-
-  return Array.from({ length: n }).map((_, i) => ({
-    timestamp: moment(date)
-      .add(30 * i, 'hour')
-      .toDate(),
-    temperature: getTemperature(),
-  }));
-};
-
-const ScreenOne = ({ navigation }) => {
-  const logs = getData(30);
-
+const Stack = () => {
   return (
-    <LinearGradient colors={['#282F46', '#4C68A3']} style={{ height: '100%' }}>
-      <Button
-        title="Go to sensor"
-        style={{ backgroundColour: 'red' }}
-        onPress={() => navigation.navigate('Sensor')}
-      />
-
-      <SensorChartRow logs={logs} />
-    </LinearGradient>
-  );
-};
-
-const Mapz = () => {
-  return <View style={{}} />;
-};
-
-const sensor = { name: 'sensor one', temperature: '3.2c' };
-
-const Sensor = () => {
-  return (
-    <LinearGradient colors={['#282F46', '#4C68A3']} style={{ height: '100%' }}>
-      <SensorChartRow sensor={sensor} logs={getData(30)} direction="left" />
-    </LinearGradient>
-  );
-};
-
-const Settings = ({ navigation }) => {
-  const [authorized] = useState(false);
-
-  const onComplete = () => {};
-
-  const isFocused = useIsFocused();
-
-  return !authorized && !__DEV__ ? (
-    isFocused && <PinEntry onComplete={onComplete} onCancel={() => navigation.goBack()} />
-  ) : (
-    <SettingsStackNavigator>
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.MENU}
-        component={MainSettingsScreen}
-      />
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.BLUETOOTH}
-        component={BluetoothSettingsScreen}
-      />
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.SENSORS}
-        component={SensorSettingsScreen}
-      />
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH}
-        component={TemperatureBreachSettingsScreen}
-      />
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.SENSOR_DETAIL}
+    <MainStackNavigator>
+      <MainStackScreen name={NAVIGATION.SCREENS.MAIN_STACK.TABS} component={SensorListScreen} />
+      <MainStackScreen
+        name={NAVIGATION.SCREENS.MAIN_STACK.SENSOR_DETAIL}
         component={SensorDetailScreen}
       />
-
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH_DETAIL}
-        component={TemperatureBreachDetailScreen}
-      />
-
-      <SettingsStackScreen
-        name={NAVIGATION.SCREENS.SETTINGS_STACK.DEVELOPER}
-        component={DevSettingsScreen}
-      />
-    </SettingsStackNavigator>
+    </MainStackNavigator>
   );
 };
-
-const Tabs = () => (
-  <MainTabNavigator>
-    <MainTabScreen name="Sensors" component={ScreenOne} />
-    <MainTabScreen name="Mapz" component={Mapz} />
-    <MainTabScreen name="Settings" component={Settings} />
-  </MainTabNavigator>
-);
 
 const App = () => {
   return (
@@ -146,12 +87,12 @@ const App = () => {
       <DependencyContainer>
         <StoreRehydrateContainer>
           <NavigationContainer>
-            <Gradient>
-              <MainStackNavigator>
-                <MainStackScreen name="Tabs" component={Tabs} />
-                <MainStackScreen name="Sensor" component={Sensor} />
-              </MainStackNavigator>
-            </Gradient>
+            <StatusBar hidden />
+            <MainHeader />
+            <MainTabNavigator>
+              <MainTabScreen name={NAVIGATION.SCREENS.MAIN_TABS.SENSORS} component={Stack} />
+              <MainTabScreen name={NAVIGATION.SCREENS.MAIN_TABS.SETTINGS} component={Settings} />
+            </MainTabNavigator>
           </NavigationContainer>
         </StoreRehydrateContainer>
       </DependencyContainer>
--- src/ui/components/navigation/index.js
+++ src/ui/components/navigation/index.js
@@ -1,4 +1,4 @@
 export { MainTabNavigator, MainTabScreen } from './MainTabNavigator';
 export { MainStackNavigator, MainStackScreen } from './MainStackNavigator';
 export { SettingsStackNavigator, SettingsStackScreen } from './SettingsStackNavigator';
-export { StackHeader } from './StackHeader';
+export { SettingsStackHeader } from './SettingsStackHeader';
