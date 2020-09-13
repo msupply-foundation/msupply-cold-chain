6e7c21a6cb14440e336598da3bcb73ae7feea95b
josh <griffinjoshua5@gmail.com>
2020-09-14 08:34:38 +1200

    Add back renamed BluetoothSlice

---
+++ src/features/Bluetooth/BluetoothSlice.js
@@ -0,0 +1,28 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { fork } from 'redux-saga/effects';
+
+import { BlinkReducer, BlinkSaga } from './Blink';
+import { DownloadReducer, DownloadSaga } from './Download';
+import { ScanReducer, ScanSaga } from './Scan';
+import { ProgramReducer, ProgramSaga } from './Program';
+import { BatteryObserverReducer, BatteryObserverSaga } from './BatteryObserver';
+
+import { REDUCER } from '~constants';
+
+export function* root() {
+  yield fork(BlinkSaga.root);
+  yield fork(DownloadSaga.root);
+  yield fork(ScanSaga.root);
+  yield fork(BatteryObserverSaga.root);
+  yield fork(ProgramSaga.root);
+}
+
+export const BluetoothReducer = combineReducers({
+  [REDUCER.BLINK]: BlinkReducer,
+  [REDUCER.DOWNLOAD]: DownloadReducer,
+  [REDUCER.SCAN]: ScanReducer,
+  [REDUCER.BATTERY_OBSERVER]: BatteryObserverReducer,
+  [REDUCER.PROGRAM]: ProgramReducer,
+});
+
+export const BluetoothSaga = { root };
