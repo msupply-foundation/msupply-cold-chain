1cf26178f7dfcb4ad3cc67f9747ae7554df60cc6
josh <griffinjoshua5@gmail.com>
2020-09-14 08:31:54 +1200

    Add remove of renamed files

--- src/features/Bluetooth/index.js
+++
@@ -1,6 +0,0 @@
-export { BatteryObserverAction, BatteryObserverSelector } from './BatteryObserver';
-export { BlinkAction, BlinkSelector } from './Blink';
-export { DownloadAction, DownloadSelector } from './Download';
-export { ProgramAction, ProgramSelector } from './Program';
-export { ScanAction, ScanSelector } from './Scan';
-export { BluetoothReducer, BluetoothSaga } from './BluetoothSlice';
--- src/features/bluetooth/index.js
+++
@@ -1,6 +0,0 @@
-export { BatteryObserverAction, BatteryObserverSelector } from './batteryObserver';
-export { BlinkAction, BlinkSelector } from './blink';
-export { DownloadAction, DownloadSelector } from './download';
-export { ProgramAction, ProgramSelector } from './Program';
-export { ScanAction, ScanSelector } from './scan';
-export { BluetoothReducer, BluetoothSaga } from './bluetoothSlice';
