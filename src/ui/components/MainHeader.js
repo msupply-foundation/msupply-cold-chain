bcc8c134d6a83667c2877c016abb2b90bc897335
josh <griffinjoshua5@gmail.com>
2020-09-15 19:33:26 +1200

    Add actual use of useTime hook

--- src/ui/components/MainHeader.js
+++ src/ui/components/MainHeader.js
@@ -2,19 +2,23 @@ import { COLOUR } from '~constants';
 
 import { Row } from '~layouts';
 import { MsupplyMan } from '~presentation/icons';
+import { useTime } from '../hooks/useTime';
 import { DateAndTime } from '../presentation/DateAndTime';
 import { BatteryStatus } from './BatteryStatus';
 
 const style = { container: { padding: 10, backgroundColor: COLOUR.HIGHLIGHT } };
 
-export const MainHeader = () => (
-  <Row style={style.container}>
-    <Row flex={2}>
-      <MsupplyMan />
+export const MainHeader = () => {
+  const time = useTime();
+  return (
+    <Row style={style.container}>
+      <Row flex={2}>
+        <MsupplyMan />
+      </Row>
+      <BatteryStatus />
+      <Row style={{}}>
+        <DateAndTime date={time} />
+      </Row>
     </Row>
-    <BatteryStatus />
-    <Row style={{}}>
-      <DateAndTime />
-    </Row>
-  </Row>
-);
+  );
+};
--- src/ui/hooks/useTime.js
+++ src/ui/hooks/useTime.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import moment from 'moment';
+import { useOnAppFocus } from './useOnAppFocus';
 
 export const useTime = () => {
   const [time, setTime] = useState(moment());
+
+  useOnAppFocus(() => setTime(moment()));
 
   useEffect(() => {
     const nextMinute = moment(time).add(1, 'minute').startOf('minute');
