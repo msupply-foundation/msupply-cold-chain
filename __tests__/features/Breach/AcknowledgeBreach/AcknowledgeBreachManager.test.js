40134297e5a85989af6a7481cce31ecd2baa5635
josh <griffinjoshua5@gmail.com>
2020-09-23 23:01:24 +1200

    Add AcknowledgeBreachManager

---
+++ __tests__/features/Breach/AcknowledgeBreach/AcknowledgeBreachManager.test.js
@@ -0,0 +1,70 @@
+import { IsNull, Not } from 'typeorm/browser';
+import { AcknowledgeBreachManager } from '~features/Breach';
+import { ENTITIES } from '~constants';
+
+describe('AcknowledgeBreachManager: getUnacknowledged', () => {
+  it('returns unacknowledged breaches', () => {
+    const unacknowledged = [{ id: 'a' }, { id: 'b' }];
+    const mockQueryWith = jest.fn(() => {
+      return unacknowledged;
+    });
+    const mockDbService = { queryWith: mockQueryWith };
+
+    const breachManager = new AcknowledgeBreachManager(mockDbService);
+
+    const result = breachManager.getUnacknowledged('a');
+
+    expect(result).resolves.toEqual(unacknowledged);
+  });
+
+  it('calls queryWith with the sensor id', () => {
+    const unacknowledged = [{ id: 'a' }, { id: 'b' }];
+    const mockQueryWith = jest.fn(() => {
+      return unacknowledged;
+    });
+    const mockDbService = { queryWith: mockQueryWith };
+
+    const breachManager = new AcknowledgeBreachManager(mockDbService);
+
+    breachManager.getUnacknowledged('a');
+
+    expect(mockQueryWith).toBeCalledTimes(1);
+    expect(mockQueryWith).toBeCalledWith(ENTITIES.TEMPERATURE_BREACH, {
+      where: { sensorId: 'a', endTimestamp: Not(IsNull()), acknowledged: false },
+    });
+  });
+});
+
+describe('AcknowledgeBreachManager: acknowledge', () => {
+  it('Returns breaches with acknowledge having been set', () => {
+    const unacknowledged = [{ id: 'a' }, { id: 'b' }];
+    const mockUpsert = jest.fn((_, entities) => entities);
+    const mockDbService = { upsert: mockUpsert };
+
+    const breachManager = new AcknowledgeBreachManager(mockDbService);
+
+    const result = breachManager.acknowledge(unacknowledged);
+    const resultShouldBe = [
+      { id: 'a', acknowledged: true },
+      { id: 'b', acknowledged: true },
+    ];
+
+    expect(result).resolves.toEqual(resultShouldBe);
+  });
+  it('Calls upsert with the correct parameters', () => {
+    const unacknowledged = [{ id: 'a' }, { id: 'b' }];
+    const mockUpsert = jest.fn((_, entities) => entities);
+    const mockDbService = { upsert: mockUpsert };
+
+    const breachManager = new AcknowledgeBreachManager(mockDbService);
+
+    breachManager.acknowledge(unacknowledged);
+    const resultShouldBe = [
+      { id: 'a', acknowledged: true },
+      { id: 'b', acknowledged: true },
+    ];
+
+    expect(mockUpsert).toBeCalledTimes(1);
+    expect(mockUpsert).toBeCalledWith(ENTITIES.TEMPERATURE_BREACH, resultShouldBe);
+  });
+});
