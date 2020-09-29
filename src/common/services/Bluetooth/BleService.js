6abe39ec4f281a3a4acd28f7a5de1ec0facb3d80
josh <griffinjoshua5@gmail.com>
2020-09-29 19:31:37 +1300

    Add BleService full refacotr

--- src/common/services/Bluetooth/BleService.js
+++ src/common/services/Bluetooth/BleService.js
@@ -1,77 +1,28 @@
-/* eslint-disable operator-assignment */
-/* eslint-disable no-bitwise */
-import Base64 from 'react-native-base64';
 import { Buffer } from 'buffer';
-import { BLUETOOTH } from '~constants';
+import { BLUE_MAESTRO, BLUETOOTH } from '~constants';
 
-const convertToSignedInt = (byteA, byteB) => {
-  const sign = byteA & (1 << 7);
-  let result = ((byteA & 0xff) << 8) | (byteB & 0xff);
-  if (sign) {
-    result = 0xffff0000 | result;
-  }
-  return result;
-};
-
-function toInt(byteArray, startPosition) {
-  return convertToSignedInt(byteArray[startPosition], byteArray[startPosition + 1]);
-}
-
-function parseDownloadedData(downloadedData) {
-  try {
-    const temperatureReadings = [];
-    const rawResultLines = downloadedData;
-
-    const totalNumberOfRecords = toInt(rawResultLines[0], 4);
-    for (let i = 1; i < rawResultLines.length; i += 1) {
-      const line = rawResultLines[i];
-
-      for (let y = 0; y < line.length; y += 2) {
-        const reading = toInt(line, y);
-        if (reading === 11308 || reading === 11776) {
-          return {
-            temperatureReadings,
-            totalNumberOfRecords,
-          };
-        }
-
-        temperatureReadings.push(reading);
-      }
-    }
-
-    return {
-      failedParsing: true,
-    };
-  } catch (e) {
-    return {
-      failedParsing: true,
-    };
-  }
-}
-
-const checkForManufacturerId = device => {
-  const { manufacturerData } = device ?? {};
-  if (!manufacturerData) return false;
-
-  const decodedBase64 = Base64.decode(manufacturerData);
-  const dataLength = decodedBase64.length;
-  const manufacturerDataArray = new Int8Array(new ArrayBuffer(dataLength));
-
-  for (let i = 0; i < dataLength; i += 1) {
-    manufacturerDataArray[i] = decodedBase64.charCodeAt(i);
-  }
-
-  return ((manufacturerDataArray[1] << 8) | manufacturerDataArray[0]) === 307;
-};
+const bufferFromBase64 = base64 => Buffer.from(base64, 'base64');
+const stringFromBase64 = base64 => bufferFromBase64(base64).toString('utf-8');
+const base64FromString = string => Buffer.from(string, 'utf-8').toString('base64');
 
 export class BleService {
   constructor(manager) {
     this.manager = manager;
-    this.downloading = {};
   }
 
   connectToDevice = async macAddress => {
-    return this.manager.connectToDevice(macAddress);
+    return this.manager.connectToDevice(macAddress, { scanMode: BLUETOOTH.SCAN_MODE_LOW_LATENCY });
+  };
+
+  connectAndDiscoverServices = async macAddress => {
+    if (await this.manager.isDeviceConnected(macAddress)) {
+      await this.manager.cancelDeviceConnection(macAddress);
+    }
+
+    const device = await this.connectToDevice(macAddress);
+    await this.manager.discoverAllServicesAndCharacteristicsForDevice(macAddress);
+
+    return device;
   };
 
   stopScan = () => {
@@ -79,257 +30,121 @@ export class BleService {
   };
 
   scanForSensors = callback => {
-    this.manager.startDeviceScan(null, { scanMode: 2 }, (_, device) => {
-      if (checkForManufacturerId(device)) {
-        callback(device);
-      }
+    this.manager.startDeviceScan(
+      null,
+      { scanMode: BLUETOOTH.SCAN_MODE_LOW_LATENCY },
+      (_, device) => {
+        const { manufacturerData } = device;
+        if (bufferFromBase64(manufacturerData).readInt16LE(0) === BLUE_MAESTRO.MANUFACTURER_ID) {
+          callback(device);
+        }
+      },
+      callback
+    );
+  };
+
+  writeCharacteristic = (macAddress, command) => {
+    return this.manager.writeCharacteristicWithoutResponseForDevice(
+      macAddress,
+      BLUETOOTH.UART_SERVICE_UUID,
+      BLUETOOTH.READ_CHARACTERISTIC_UUID,
+      base64FromString(command)
+    );
+  };
+
+  monitorCharacteristic = (macAddress, callback) => {
+    return new Promise((resolve, reject) => {
+      this.manager.monitorCharacteristicForDevice(
+        macAddress,
+        BLUETOOTH.UART_SERVICE_UUID,
+        BLUETOOTH.WRITE_CHARACTERISTIC_UUID,
+        (_, result) => {
+          callback(result, resolve, reject);
+        }
+      );
     });
   };
 
-  downloadLogs = async macAddress => {
-    if (this.downloading[macAddress]) throw new Error('Already downloading');
-    this.downloading[macAddress] = true;
-
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolver, reject) => {
-      try {
-        const device = await this.connectToDevice(macAddress);
-
+  writeAndMonitor = async (macAddress, command, parser) => {
+    const monitor = this.monitorCharacteristic(
+      macAddress,
+      (() => {
         const data = [];
-        const onDisconnectedSubscription = device.onDisconnected(async () => {
-          try {
-            const parsedData = parseDownloadedData(data);
-            resolver(
-              parsedData.temperatureReadings.map(temp => ({ temperature: temp / 10.0 })) ?? []
-            );
-          } catch (error) {
-            reject(error);
-          } finally {
-            onDisconnectedSubscription.remove();
-            this.downloading[macAddress] = false;
-          }
-        });
 
-        await device.discoverAllServicesAndCharacteristics();
-        await this.manager.monitorCharacteristicForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.WRITE_CHARACTERISTIC_UUID,
-          (_, characteristic) => {
-            if (characteristic?.value) {
-              try {
-                const raw = Base64.decode(characteristic.value);
-                const rawLength = raw.length;
-                const array = new Int16Array(new ArrayBuffer(rawLength * 2));
-                for (let i = 0; i < rawLength; i += 1) array[i] = raw.charCodeAt(i);
-                data.push(array);
-              } catch (e) {
-                reject(e);
-              } finally {
-                this.downloading[macAddress] = false;
-              }
+        return (result, resolve, reject) => {
+          if (result?.value) data.push(result.value);
+          else {
+            try {
+              resolve(parser(data));
+            } catch (e) {
+              reject(new Error(`Parsing failed:  ${e.message}`));
             }
           }
-        );
+        };
+      })()
+    );
 
-        await this.manager.writeCharacteristicWithoutResponseForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.READ_CHARACTERISTIC_UUID,
-          Buffer.from('*logall', 'utf-8').toString('base64')
-        );
-      } catch (error) {
-        reject(error);
-      } finally {
-        this.downloading[macAddress] = false;
-      }
+    await this.writeCharacteristic(macAddress, command);
+
+    return monitor;
+  };
+
+  sendCommand = async (macAddress, command, parser) => {
+    await this.connectAndDiscoverServices(macAddress);
+    return this.writeAndMonitor(macAddress, command, parser);
+  };
+
+  downloadLogs = async macAddress => {
+    return this.sendCommand(macAddress, BLUE_MAESTRO.COMMANDS.DOWNLOAD, data => {
+      const buffer = Buffer.concat(data.slice(1).map(datum => bufferFromBase64(datum)));
+
+      const ind = buffer.findIndex(
+        (_, i) =>
+          (i % 2 === 0 && buffer.readInt16BE(i) === BLUE_MAESTRO.DELIMITER_A) ||
+          buffer.readInt16BE(i) === BLUE_MAESTRO.DELIMITER_B
+      );
+
+      return buffer.slice(0, ind).reduce((acc, _, index, buf) => {
+        if (index % 2 !== 0) return acc;
+        return [...acc, { temperature: buf.readInt16BE(index) / BLUE_MAESTRO.TEMPERATURE_DIVISOR }];
+      }, []);
     });
   };
 
   updateLogInterval = async (macAddress, logInterval) => {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolver, reject) => {
-      try {
-        const device = await this.connectToDevice(macAddress);
-        await device.discoverAllServicesAndCharacteristics();
-
-        const monitorSubscription = await this.manager.monitorCharacteristicForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.WRITE_CHARACTERISTIC_UUID,
-
-          (_, result) => {
-            if (result?.value) {
-              const raw = Base64.decode(result.value);
-              const rawLength = raw.length;
-              const array = new Uint8Array(new ArrayBuffer(rawLength));
-
-              for (let i = 0; i < rawLength; i += 1) array[i] = raw.charCodeAt(i);
-
-              resolver(array);
-
-              monitorSubscription.remove();
-              device.cancelConnection();
-            }
-          }
-        );
-
-        this.manager.writeCharacteristicWithoutResponseForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.READ_CHARACTERISTIC_UUID,
-          Buffer.from(`*lint${logInterval}`, 'utf-8').toString('base64')
-        );
-      } catch (error) {
-        reject(error);
+    return this.sendCommand(
+      macAddress,
+      `${BLUE_MAESTRO.COMMANDS.UPDATE_LOG_INTERVAL}${logInterval}`,
+      data => {
+        const [datum] = data;
+        return stringFromBase64(datum);
       }
-    });
+    );
   };
 
   blink = async macAddress => {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolve, reject) => {
-      try {
-        const device = await this.connectToDevice(macAddress, { scanMode: 2 });
-
-        const onDisconnectedSubscription = device.onDisconnected(() => {
-          reject();
-          onDisconnectedSubscription.remove();
-        });
-        await device.discoverAllServicesAndCharacteristics();
-
-        const onMonitorSubscription = await this.manager.monitorCharacteristicForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.WRITE_CHARACTERISTIC_UUID,
-
-          (_, result) => {
-            const asString = Buffer.from(result?.value ?? '', 'base64').toString('ascii');
-            if (asString.match(/ok/i)) {
-              resolve(true);
-            } else {
-              reject(new Error('Decode failed'));
-            }
-
-            onMonitorSubscription.remove();
-            device.cancelConnection();
-          }
-        );
-
-        await this.manager.writeCharacteristicWithoutResponseForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.READ_CHARACTERISTIC_UUID,
-          Buffer.from('*blink', 'utf-8').toString('base64')
-        );
-      } catch (e) {
-        reject(e);
-      }
+    return this.sendCommand(macAddress, BLUE_MAESTRO.COMMANDS.BLINK, data => {
+      const [datum] = data;
+      return !!stringFromBase64(datum).match(/ok/i);
     });
   };
 
   getInfo = async macAddress => {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolver, reject) => {
-      try {
-        const device = await this.connectToDevice(macAddress);
-
-        const data = [];
-        const onDisconnectedSubscription = device.onDisconnected(async () => {
-          try {
-            if (!data.length) throw new Error('No data!');
-            const parsedAsFullString = data
-              .reduce((acc, value) => acc.concat(...value), [])
-              .map(d => String.fromCharCode(d))
-              .join('');
-
-            const batteryLevel = Number(
-              ((parsedAsFullString.match(/Batt lvl: [0-9]{1,3}/) ?? [''])[0].match(
-                /[0-9]{1,3}/
-              ) ?? [''])[0] ?? 0
-            );
-
-            const isDisabled = !!parsedAsFullString.match(/Btn on\/off: 1/);
-            const result = { batteryLevel, isDisabled };
-
-            resolver(result);
-          } catch (error) {
-            reject(error);
-          }
-          this.downloading[macAddress] = false;
-          onDisconnectedSubscription.remove();
-        });
-
-        await device.discoverAllServicesAndCharacteristics();
-        await this.manager.monitorCharacteristicForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.WRITE_CHARACTERISTIC_UUID,
-          (_, characteristic) => {
-            if (characteristic?.value) {
-              const raw = Base64.decode(characteristic.value);
-              const rawLength = raw.length;
-              const array = new Int16Array(new ArrayBuffer(rawLength * 2));
-              for (let i = 0; i < rawLength; i += 1) array[i] = raw.charCodeAt(i);
-              data.push(array);
-            }
-          }
-        );
-
-        await this.manager.writeCharacteristicWithoutResponseForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.READ_CHARACTERISTIC_UUID,
-          Buffer.from('*info', 'utf-8').toString('base64')
-        );
-      } catch (error) {
-        this.downloading[macAddress] = false;
-
-        reject(error);
-      }
-    });
+    return this.sendCommand(macAddress, BLUE_MAESTRO.COMMANDS.INFO, data =>
+      data
+        .map(datum => stringFromBase64(datum))
+        .reduce((acc, info) => {
+          if (info.match(/Batt/)) return { ...acc, batteryLevel: info.match(/[0-9]{1,3}/)[0] };
+          if (info.match(/Btn on\/off: 1/)) return { ...acc, isDisabled: true };
+          return acc;
+        }, {})
+    );
   };
 
   toggleButton = async macAddress => {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolve, reject) => {
-      try {
-        const device = await this.connectToDevice(macAddress, { scanMode: 2 });
-
-        const disconnectedSubscription = device.onDisconnected(() => {
-          reject();
-          disconnectedSubscription.remove();
-        });
-
-        await device.discoverAllServicesAndCharacteristics();
-        await this.manager.monitorCharacteristicForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.WRITE_CHARACTERISTIC_UUID,
-
-          (_, result) => {
-            if (result?.value) {
-              const raw = Base64.decode(result.value);
-              const rawLength = raw.length;
-              const array = new Uint8Array(new ArrayBuffer(rawLength));
-
-              for (let i = 0; i < rawLength; i += 1) array[i] = raw.charCodeAt(i);
-
-              resolve(array);
-              device.cancelConnection();
-            }
-          }
-        );
-
-        await this.manager.writeCharacteristicWithoutResponseForDevice(
-          macAddress,
-          BLUETOOTH.UART_SERVICE_UUID,
-          BLUETOOTH.READ_CHARACTERISTIC_UUID,
-          Buffer.from('*bd', 'utf-8').toString('base64')
-        );
-      } catch (e) {
-        reject(e);
-      }
+    return this.sendCommand(macAddress, BLUE_MAESTRO.COMMANDS.DISABLE_BUTTON, data => {
+      const [datum] = data;
+      return !!stringFromBase64(datum).match(/ok/i);
     });
   };
 
