39ae2ec92053e3c23730aa0c594311670a17987b
josh <griffinjoshua5@gmail.com>
2020-11-30 20:14:37 +1300

    Add a delay to load after interactions

--- src/ui/hoc/withLoadAfterInteractions.js
+++ src/ui/hoc/withLoadAfterInteractions.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 
 import { COLOUR } from '~constants';
@@ -7,12 +8,17 @@ import { Centered } from '../layouts';
 
 export const withLoadAfterInteractions = WrappedComponent => props => {
   const load = useLoadAfterInteractions();
+  const delay = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => useState(false), 300);
+  }, [load]);
 
   return load ? (
     <WrappedComponent {...props} />
   ) : (
     <Centered style={{ width: '100%', height: '100%' }}>
-      <ActivityIndicator size="large" color={COLOUR.PRIMARY} />
+      {delay ? null : <ActivityIndicator size="large" color={COLOUR.PRIMARY} />}
     </Centered>
   );
 };
