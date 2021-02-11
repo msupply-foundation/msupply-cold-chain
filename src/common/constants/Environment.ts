a2e81dda68843d64ae83d3d787e729f2f85137d8
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-11 21:50:02 +1300

    Add constants and translations as ts files

--- src/common/constants/Environment.ts
+++ src/common/constants/Environment.ts
@@ -1,4 +1,4 @@
-import env from '~/env.json';
+import * as env from '../../../env.json';
 
 const ENVIRONMENT = env;
 
--- src/common/translations/index.ts
+++ src/common/translations/index.ts
@@ -1,14 +1,14 @@
 import i18n from 'i18n-js';
 import * as RNLocalize from 'react-native-localize';
 
-const { languageTag } = RNLocalize.findBestAvailableLanguage(['en']);
+const { languageTag } = RNLocalize.findBestAvailableLanguage(['en']) ?? {};
 
 const AVAILABLE_TRANSLATIONS = { en: require('./en.json') };
 
 i18n.fallbacks = true;
 i18n.translations = AVAILABLE_TRANSLATIONS;
 
-export const t = (scope, options) => {
+export const t = (scope: i18n.Scope, options: i18n.TranslateOptions) => {
   const translation = i18n.t(scope, { languageTag, ...options });
 
   // Catch missing translations - throw errors in dev, but return an empty string in
