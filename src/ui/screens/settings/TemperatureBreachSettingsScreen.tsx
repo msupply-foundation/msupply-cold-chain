8b687f3d1ebeb55e27d0f47f1137df60ca53bd98
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-26 09:29:58 +1300

    Add update to types for TemperatureBreachSettingsScreen

--- src/ui/screens/settings/TemperatureBreachSettingsScreen.tsx
+++ src/ui/screens/settings/TemperatureBreachSettingsScreen.tsx
@@ -1,19 +1,26 @@
 import { ActivityIndicator } from 'react-native';
-
-import { useEffect } from 'react';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { FC, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { t } from '~translations';
-import { COLOUR, NAVIGATION } from '~constants';
-import { BreachConfigurationSelector, BreachConfigurationAction } from '~features/Entities';
+import { t } from '../../../common/translations';
+import { COLOUR, SETTINGS_STACK } from '../../../common/constants';
+import { BreachConfigurationSelector, BreachConfigurationAction } from '../../../features/Entities';
 
-import { SettingsList } from '~layouts';
-import { SettingsGroup, SettingsNavigationRow } from '~components/settings';
+import { SettingsList } from '../../layouts';
+import { SettingsGroup, SettingsNavigationRow } from '../../components/settings';
+import { SettingsStackParameters } from '../../containers/SettingsStackNavigator';
 
-const BREACH_DETAIL_SCREEN = NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_BREACH_DETAIL;
-const CUMULATIVE_DETAIL_SCREEN = NAVIGATION.SCREENS.SETTINGS_STACK.TEMPERATURE_CUMULATIVE_DETAIL;
+const BREACH_DETAIL_SCREEN = SETTINGS_STACK.TEMPERATURE_BREACH_DETAIL;
+const CUMULATIVE_DETAIL_SCREEN = SETTINGS_STACK.TEMPERATURE_CUMULATIVE_DETAIL;
 
-export const TemperatureBreachSettingsScreen = ({ navigation }) => {
+type TemperatureBreachSettingsScreenProps = {
+  navigation: StackNavigationProp<SettingsStackParameters, SETTINGS_STACK.MENU>;
+};
+
+export const TemperatureBreachSettingsScreen: FC<TemperatureBreachSettingsScreenProps> = ({
+  navigation,
+}) => {
   const hotBreach = useSelector(BreachConfigurationSelector.hotBreachConfig);
   const coldBreach = useSelector(BreachConfigurationSelector.coldBreachConfig);
   const hotCumulative = useSelector(BreachConfigurationSelector.hotCumulativeConfig);
@@ -29,11 +36,15 @@ export const TemperatureBreachSettingsScreen = ({ navigation }) => {
     <SettingsList>
       <SettingsGroup title={t('SINGLE_EXPOSURE_CONFIGS')}>
         <SettingsNavigationRow
+          Icon={null}
+          subtext=""
           key={hotBreach.id}
           label={hotBreach.description}
           onPress={() => navigation.navigate(BREACH_DETAIL_SCREEN, { id: hotBreach.id })}
         />
         <SettingsNavigationRow
+          Icon={null}
+          subtext=""
           key={coldBreach.id}
           label={coldBreach.description}
           onPress={() => navigation.navigate(BREACH_DETAIL_SCREEN, { id: coldBreach.id })}
@@ -41,11 +52,15 @@ export const TemperatureBreachSettingsScreen = ({ navigation }) => {
       </SettingsGroup>
       <SettingsGroup title={t('CUMULATIVE_EXPOSURE_CONFIGS')}>
         <SettingsNavigationRow
+          Icon={null}
+          subtext=""
           key={hotCumulative.id}
           label={hotCumulative.description}
           onPress={() => navigation.navigate(CUMULATIVE_DETAIL_SCREEN, { id: hotCumulative.id })}
         />
         <SettingsNavigationRow
+          Icon={null}
+          subtext=""
           key={coldCumulative.id}
           label={coldCumulative.description}
           onPress={() => navigation.navigate(CUMULATIVE_DETAIL_SCREEN, { id: coldCumulative.id })}
