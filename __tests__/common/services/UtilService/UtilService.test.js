a16d6b07d6fe623b99e464384bf1219c627a1e42
Joshua Griffin <griffinjoshua5@gmail.com>
2021-05-14 22:30:16 +1200

    Add get versionCode to utilService

--- __tests__/common/services/UtilService/UtilService.test.js
+++ __tests__/common/services/UtilService/UtilService.test.js
@@ -7,3 +7,16 @@ describe('Return value', () => {
     expect(utils.uuid()).toEqual('1');
   });
 });
+
+describe('getVersionCode', () => {
+  it('returns the correct format', () => {
+    const utils = new UtilService();
+
+    expect(utils.getVersionCode('0.0.3')).toEqual(399);
+    expect(utils.getVersionCode('0.0.4')).toEqual(499);
+    expect(utils.getVersionCode('0.1.4')).toEqual(100499);
+    expect(utils.getVersionCode('1.1.4')).toEqual(10100499);
+    expect(utils.getVersionCode('1.1.4-rc1')).toEqual(10100401);
+    expect(utils.getVersionCode('0.0.4-rc98')).toEqual(10100498);
+  });
+});
