1c91cc8b857d29e599deaae8636235cc618b4972
Joshua Griffin <griffinjoshua5@gmail.com>
2021-05-14 15:31:41 +1200

    Add updated migration code

--- src/common/services/Database/migrations/0_0_3.ts
+++ src/common/services/Database/migrations/0_0_3.ts
@@ -7,7 +7,9 @@ export const migration0_0_3 = {
   // - Changes sensor.macAddress to a NON unique field. This is so we can remove and reconnect sensors with different IDs.
   // - Adds sensor.isActive so we have a soft delete mechanism for sensors.
   migrate: async (dbService: DatabaseService): Promise<void> => {
-    const sensors = await dbService.getAll(ENTITIES.SENSOR);
+    const sensors = await dbService.query(
+      'select id, logInterval, batteryLevel, programmedDate, logDelay, name, macAddress from Sensor'
+    );
     const createSensorTableQuery = Sensor.getTableDefinition();
 
     await dbService.query('PRAGMA foreign_keys=OFF');
