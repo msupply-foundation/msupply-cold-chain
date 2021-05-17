2189eedea89476c9e64ca29cc0fb60d4fe63618f
Joshua Griffin <griffinjoshua5@gmail.com>
2021-05-18 08:54:14 +1200

    Add updated SyncOutManager and SyncQueueManager typings

--- src/features/Sync/SyncOutManager.ts
+++ src/features/Sync/SyncOutManager.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import { ENTITIES } from '~constants';
+import { TemperatureLog } from '~services/Database/entities';
+import { Sensor, TemperatureBreach } from '~common/services/Database/entities';
+import { Syncable } from '~features/Sync/SyncQueueManager';
 
-const mapBreaches = (breaches: any[]) => {
-  return breaches.map((breach: any) => {
+const mapBreaches = (breaches: TemperatureBreach[]): TemperatureBreachSyncOut[] => {
+  return breaches.map((breach: TemperatureBreach) => {
     const {
       id,
       temperatureBreachConfiguration,
@@ -23,7 +27,7 @@ const mapBreaches = (breaches: any[]) => {
     const thresholdMaximumTemperature = maximumTemperature;
     const thresholdDuration = duration;
 
-    const mapped = {
+    const mapped: TemperatureBreachSyncOut = {
       type,
       thresholdMinimumTemperature,
       thresholdMaximumTemperature,
@@ -42,11 +46,40 @@ const mapBreaches = (breaches: any[]) => {
   });
 };
 
+type SyncableDataTypes = 'TemperatureLog' | 'TemperatureBreach' | 'Sensor';
+
+type TemperatureLogSyncOut = TemperatureLog;
+type SensorSyncOut = Sensor;
+type TemperatureBreachSyncOut = {
+  type: string;
+  thresholdMinimumTemperature: number;
+  thresholdMaximumTemperature: number;
+  thresholdDuration: number;
+  id: string;
+  sensorId: string;
+  startTimestamp: number;
+  acknowledged: boolean;
+  endTimestamp?: number;
+};
+
+type SyncOut = TemperatureLogSyncOut | SensorSyncOut | TemperatureBreachSyncOut;
+
+const mapInternalDataTypeToSyncable = (type: SyncableDataTypes, records: Syncable[]): SyncOut[] => {
+  switch (type) {
+    case 'TemperatureLog':
+      return records as TemperatureLogSyncOut[];
+    case 'TemperatureBreach':
+      return mapBreaches(records as TemperatureBreach[]) as TemperatureBreachSyncOut[];
+    case 'Sensor':
+      return records as Sensor[];
+  }
+};
+
 class SyncOutManager {
   private getAuthenticationBody = (username: string, password: string): string =>
     JSON.stringify({ username, password });
 
-  private getSyncBody = (logs: any[]): string => {
+  private getSyncBody = (logs: SyncOut[]): string => {
     return JSON.stringify(logs);
   };
 
@@ -55,17 +88,32 @@ class SyncOutManager {
       withCredentials: true,
     });
 
-  public syncSensors = async (sensorUrl: string, logs: any[]): Promise<any> =>
-    axios.put(sensorUrl, this.getSyncBody(logs), { withCredentials: true });
+  public syncSensors = async (sensorUrl: string, logs: Sensor[]): Promise<any> =>
+    axios.put(sensorUrl, this.getSyncBody(mapInternalDataTypeToSyncable(ENTITIES.SENSOR, logs)), {
+      withCredentials: true,
+    });
 
-  public syncTemperatureLogs = async (temperatureLogUrl: string, logs: any[]): Promise<void> =>
-    axios.put(temperatureLogUrl, this.getSyncBody(logs), { withCredentials: true });
+  public syncTemperatureLogs = async (
+    temperatureLogUrl: string,
+    logs: TemperatureLog[]
+  ): Promise<void> =>
+    axios.put(
+      temperatureLogUrl,
+      this.getSyncBody(mapInternalDataTypeToSyncable(ENTITIES.TEMPERATURE_LOG, logs)),
+      { withCredentials: true }
+    );
 
   public syncTemperatureBreaches = async (
     temperatureBreachUrl: string,
-    logs: any[]
+    logs: TemperatureBreach[]
   ): Promise<void> =>
-    axios.put(temperatureBreachUrl, this.getSyncBody(mapBreaches(logs)), { withCredentials: true });
+    axios.put(
+      temperatureBreachUrl,
+      this.getSyncBody(mapInternalDataTypeToSyncable(ENTITIES.TEMPERATURE_BREACH, logs)),
+      {
+        withCredentials: true,
+      }
+    );
 }
 
 export { SyncOutManager };
--- src/features/Sync/SyncQueueManager.ts
+++ src/features/Sync/SyncQueueManager.ts
@@ -1,8 +1,12 @@
+import { TemperatureLog } from '~services/Database/entities';
+import { Sensor, TemperatureBreach } from '~common/services/Database/entities';
 import _ from 'lodash';
 import { In } from 'typeorm/browser';
 import { ENTITIES } from '../../common/constants';
-import { DatabaseService, UtilService } from '../../common/services';
+import { DatabaseService } from '../../common/services';
 import { SyncLog } from '../../common/services/Database/entities';
+
+export type Syncable = Sensor | TemperatureLog | TemperatureBreach;
 
 const SYNC_QUEUE_PEEK_NEXT = `
     SELECT * FROM synclog
@@ -10,24 +14,6 @@ const SYNC_QUEUE_PEEK_NEXT = `
     ORDER BY timestamp
     LIMIT ?
 `;
-
-const SYNC_QUEUE_PEEK_ALL = `
-    SELECT * FROM synclog 
-    WHERE type = ? 
-    ORDER BY timestamp
-`;
-
-// const SYNC_QUEUE_DROP_NEXT = `
-//   DELETE FROM synclog
-//   WHERE synclog.id IN
-//   (
-//     SELECT id
-//     FROM synclog
-//     WHERE type = ?
-//     ORDER BY timestamp
-//     LIMIT ?
-//   )
-// `;
 
 const SYNC_QUEUE_DROP = `
   DELETE FROM synclog
@@ -46,55 +32,40 @@ const SYNC_QUEUE_LENGTH_FOR_TYPE = `
 class SyncQueueManager {
   private databaseService: DatabaseService;
 
-  private utilService: UtilService;
-
-  constructor(databaseService: DatabaseService, utilService: UtilService) {
+  constructor(databaseService: DatabaseService) {
     this.databaseService = databaseService;
-    this.utilService = utilService;
   }
-
-  pushLog = async (id: string, type: string, payload: string): Promise<SyncLog> => {
-    return this.databaseService.save(ENTITIES.SYNC_LOG, {
-      id,
-      type,
-      payload,
-      timestamp: this.utilService.now(),
-    });
-  };
 
   nextSyncLogs = async (entity: string, count = 999): Promise<SyncLog[]> => {
     return this.databaseService.query(SYNC_QUEUE_PEEK_NEXT, [entity, count]);
   };
 
-  nextSensors = async (count = 999): Promise<SyncLog[]> => {
+  nextSensors = async (count = 999): Promise<Sensor[]> => {
     const syncOuts = await this.nextSyncLogs(ENTITIES.SENSOR, count);
-    const sensors = await this.databaseService.queryWith(ENTITIES.SENSOR, {
+    return await this.databaseService.queryWith(ENTITIES.SENSOR, {
       where: { id: In(syncOuts.map(({ id }) => id)) },
     });
-    return sensors;
   };
 
-  nextTemperatureLogs = async (count = 999): Promise<SyncLog[]> => {
+  nextTemperatureLogs = async (count = 999): Promise<TemperatureLog[]> => {
     const syncOuts = await this.nextSyncLogs(ENTITIES.TEMPERATURE_LOG, count);
-    const temperatureLogs = await this.databaseService.queryWith(ENTITIES.TEMPERATURE_LOG, {
+    return await this.databaseService.queryWith(ENTITIES.TEMPERATURE_LOG, {
       where: { id: In(syncOuts.map(({ id }) => id)) },
     });
-    return temperatureLogs;
   };
 
-  nextTemperatureBreaches = async (count = 999): Promise<SyncLog[]> => {
+  nextTemperatureBreaches = async (count = 999): Promise<TemperatureBreach[]> => {
     const syncOuts = await this.nextSyncLogs(ENTITIES.TEMPERATURE_BREACH, count);
-    const breaches = await this.databaseService.queryWith(ENTITIES.TEMPERATURE_BREACH, {
+    return await this.databaseService.queryWith(ENTITIES.TEMPERATURE_BREACH, {
       where: { id: In(syncOuts.map(({ id }) => id)) },
     });
-    return breaches;
   };
 
-  dropLogs = async (logs: any[]): Promise<void> => {
-    const chunks: any[][] = _.chunk(logs, 100);
+  dropLogs = async (logs: Syncable[]): Promise<void> => {
+    const chunks: Syncable[][] = _.chunk(logs, 100);
 
     const queries: string[][] = chunks.map((chunk: any) => {
-      return chunk.map(({ id }: any) => {
+      return chunk.map(({ id }: Syncable) => {
         const qb = this.databaseService.getQueryBuilder();
         return qb.delete().from('SyncLog').where('id = :id', { id }).getQueryAndParameters();
       });
