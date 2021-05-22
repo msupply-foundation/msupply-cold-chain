c248bdc9dbd837eeb5a18cea5b34d1820cc409f4
Joshua Griffin <griffinjoshua5@gmail.com>
2021-05-22 21:57:18 +1200

    Add rootreducer import fixes

--- src/common/store/RootReducer.ts
+++ src/common/store/RootReducer.ts
@@ -1,17 +1,15 @@
 import { combineReducers } from 'redux';
-
 import { REDUCER } from '../constants';
-
-import { EntitiesReducer } from '../../features/Entities';
-import { ChartReducer } from '../../features/Chart';
-import { BreachReducer } from '../../features/Breach';
-import { SensorDetailReducer } from '../../features/SensorDetail';
-import { BluetoothReducer } from '../../features/Bluetooth';
-import { ReportReducer } from '../../features/Report';
-import { SensorStatusReducer } from '../../features/SensorStatus';
-import { PermissionReducer } from '../../features/Permission';
-import { SyncReducer } from '../../features/Sync';
-import { DevReducer } from '../../features/Dev';
+import { EntitiesReducer } from '~features/Entities';
+import { ChartReducer } from '~features/Chart';
+import { BreachReducer } from '~features/Breach';
+import { SensorDetailReducer } from '~features/SensorDetail';
+import { BluetoothReducer } from '~features/Bluetooth';
+import { ReportReducer } from '~features/Report';
+import { SensorStatusReducer } from '~features/SensorStatus';
+import { PermissionReducer } from '~features/Permission';
+import { SyncReducer } from '~features/Sync';
+import { DevReducer } from '~features/Dev';
 
 export const RootReducer = combineReducers({
   [REDUCER.ENTITIES]: EntitiesReducer,
