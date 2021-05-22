1b54bca8775ce3e0be9fb64b570f1391b3bb18e5
Joshua Griffin <griffinjoshua5@gmail.com>
2021-05-22 21:57:09 +1200

    Add environment typing corrections

--- src/common/constants/Environment.ts
+++ src/common/constants/Environment.ts
@@ -1,7 +1,7 @@
-import { env, MOCK_BLE, REDUX_LOGGER, DEV_LOGGER, QUERY_LOGGER } from 'react-native-dotenv';
+import { MOCK_BLE, REDUX_LOGGER, DEV_LOGGER, QUERY_LOGGER } from 'react-native-dotenv';
 
 type EnvironmentKey = 'MOCK_BLE' | 'DEV_LOGGER' | 'QUERY_LOGGER' | 'REDUX_LOGGER';
-type ImportedEnvironmentShape = Record<EnvironmentKey, string>;
+type ImportedEnvironmentShape = Record<EnvironmentKey, 'true' | 'false'>;
 type EnvironmentShape = Record<EnvironmentKey, boolean | string>;
 
 const ENV_VARS: ImportedEnvironmentShape = { MOCK_BLE, REDUX_LOGGER, DEV_LOGGER, QUERY_LOGGER };
@@ -13,9 +13,8 @@ const ENVIRONMENT: EnvironmentShape = {
   QUERY_LOGGER: false,
 };
 
-Object.keys(ENV_VARS).forEach(key => {
-  // TODO: Can this be typed better?
-  const envKey = key as EnvironmentKey;
+(Object.keys(ENV_VARS) as Array<keyof typeof ENV_VARS>).forEach(key => {
+  const envKey = key;
   const envVal = ENV_VARS[envKey];
   if (envVal.toLowerCase() === 'true' || envVal.toLowerCase() === 'false') {
     ENVIRONMENT[envKey] = Boolean(envVal.toLowerCase() === 'true');
---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native-dotenv' {
+  export const MOCK_BLE: 'true' | 'false';
+  export const REDUX_LOGGER: 'true' | 'false';
+  export const DEV_LOGGER: 'true' | 'false';
+  export const QUERY_LOGGER: 'true' | 'false';
+}
