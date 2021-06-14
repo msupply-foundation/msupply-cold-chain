602ec046b60cfcd58dab69c690af66518b2423a3
Joshua Griffin <griffinjoshua5@gmail.com>
2021-06-15 10:37:11 +1200

    Add class transforming for serializable state

--- src/features/Breach/AcknowledgeBreach/AcknowledgeBreachManager.ts
+++ src/features/Breach/AcknowledgeBreach/AcknowledgeBreachManager.ts
@@ -1,5 +1,6 @@
-import { TemperatureBreach } from '~services/Database/entities';
+import { classToPlain } from 'class-transformer';
 import { IsNull, Not } from 'typeorm/browser';
+import { TemperatureBreach } from '~services/Database/entities';
 import { ENTITIES } from '~common/constants';
 import { DatabaseService } from '~common/services';
 
@@ -11,13 +12,16 @@ export class AcknowledgeBreachManager {
   }
 
   getUnacknowledged = async (sensorId: string): Promise<TemperatureBreach[]> => {
-    return this.databaseService.queryWith(ENTITIES.TEMPERATURE_BREACH, {
+    const unacknowledged = await this.databaseService.queryWith(ENTITIES.TEMPERATURE_BREACH, {
       where: { sensorId, endTimestamp: Not(IsNull()), acknowledged: false },
     });
+
+    return unacknowledged.map((breach: TemperatureBreach) => classToPlain(breach));
   };
 
   acknowledge = async (breaches: TemperatureBreach[]): Promise<TemperatureBreach[]> => {
     const acknowledged = breaches.map(breach => ({ ...breach, acknowledged: true }));
-    return this.databaseService.upsert(ENTITIES.TEMPERATURE_BREACH, acknowledged);
+    const updated = await this.databaseService.upsert(ENTITIES.TEMPERATURE_BREACH, acknowledged);
+    return updated.map((breach: TemperatureBreach) => classToPlain(breach));
   };
 }
