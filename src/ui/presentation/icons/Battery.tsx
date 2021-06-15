143b90cf697be8c208557d09109063d67f04eba8
Joshua Griffin <griffinjoshua5@gmail.com>
2021-06-15 22:32:57 +1200

    Add correct battery thresholds

--- src/ui/presentation/icons/Battery.tsx
+++ src/ui/presentation/icons/Battery.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { TextStyle } from 'react-native';
 import { COLOUR } from '~common/constants';
+import { BatteryLevelThreshold } from '~features/SensorStatus';
 import { ICON_NAME, Icon, ICON_SIZE } from './Icon';
 
 interface BatteryProps extends TextStyle {
@@ -18,10 +19,10 @@ export const Battery: FC<BatteryProps> = ({
 }) => {
   const getIconProps = (level: number, charging: boolean): [ICON_NAME, COLOUR] => {
     if (charging) return [ICON_NAME.BATTERY_CHARGING, COLOUR.PRIMARY];
-    if (level < 15) return [ICON_NAME.BATTERY_EMPTY, COLOUR.DANGER];
-    if (level < 40) return [ICON_NAME.BATTERY_ONE, COLOUR.DANGER];
-    if (level < 65) return [ICON_NAME.BATTERY_TWO, COLOUR.PRIMARY];
-    if (level < 90) return [ICON_NAME.BATTERY_THREE, COLOUR.PRIMARY];
+    if (level < BatteryLevelThreshold.Empty) return [ICON_NAME.BATTERY_EMPTY, COLOUR.DANGER];
+    if (level < BatteryLevelThreshold.Low) return [ICON_NAME.BATTERY_ONE, COLOUR.DANGER];
+    if (level < BatteryLevelThreshold.Medium) return [ICON_NAME.BATTERY_TWO, COLOUR.PRIMARY];
+    if (level < BatteryLevelThreshold.High) return [ICON_NAME.BATTERY_THREE, COLOUR.PRIMARY];
     return [ICON_NAME.BATTERY_FOUR, COLOUR.PRIMARY];
   };
 
