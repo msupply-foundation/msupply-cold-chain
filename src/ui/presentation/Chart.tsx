04ff0881b34b170b9fd6b015e9a6bda4ea3d4cc3
Joshua Griffin <griffinjoshua5@gmail.com>
2021-06-21 11:41:38 +1200

    Add updated chart status

--- src/ui/presentation/Chart.tsx
+++ src/ui/presentation/Chart.tsx
@@ -1,14 +1,13 @@
 import React, { FC } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { VictoryLine, VictoryChart, VictoryAxis } from 'victory-native';
-import { FONT, STYLE, COLOUR, CHART, DEPENDENCY } from '../../common/constants';
-
+import { FONT, STYLE, COLOUR, CHART, DEPENDENCY } from '~constants';
 import { ChartGradient } from './ChartGradient';
-import { Centered, Row } from '../layouts';
+import { Centered, Row } from '~layouts';
 import { MediumText } from './typography';
-import { FormatService } from '../../common/services';
-import { ChartDataPoint } from '../../features/Chart/ChartManager';
-import { useDependency } from '../hooks';
+import { FormatService } from '~common/services';
+import { ChartDataPoint } from '~features/Chart';
+import { useDependency } from '~hooks';
 
 const style = {
   container: {
@@ -62,10 +61,46 @@ export const Chart: FC<ChartProps> = ({ data = [], isLoading, width, height }) =
   const minTime = Math.min(...data.map(({ timestamp }) => timestamp));
   const maxTime = Math.max(...data.map(({ timestamp }) => timestamp));
 
-  const Empty = (
+  const Empty = () => (
     <Row alignItems="center" justifyContent="center" style={{ width: STYLE.WIDTH.NORMAL_CHART }}>
       <MediumText>No data</MediumText>
     </Row>
+  );
+
+  const FullChart = () => (
+    <VictoryChart
+      width={width}
+      height={height}
+      padding={{ top: 10, bottom: 30, left: 30, right: 30 }}
+      domain={{ y: [domainMin, domainMax], x: [minTime, maxTime] }}
+    >
+      {/* X AXIS */}
+      <VictoryAxis
+        orientation="bottom"
+        offsetY={offsetY}
+        style={style.xAxis}
+        tickFormat={tickFormatter}
+        tickCount={5}
+      />
+
+      {/* Y AXIS */}
+      <VictoryAxis dependentAxis orientation="left" tickCount={5} style={style.yAxis} />
+
+      <ChartGradient />
+      <VictoryLine
+        data={data}
+        x={CHART.X_KEY}
+        y={CHART.Y_KEY}
+        interpolation={CHART.INTERPOLATION}
+        style={style.line}
+      />
+    </VictoryChart>
+  );
+
+  const LoadingIndicator = () => (
+    <Centered style={{ width: '100%', minHeight: height }}>
+      <ActivityIndicator size="large" color={COLOUR.PRIMARY} />
+    </Centered>
   );
 
   // currently a small bug with the x-axis placement when values are both +ve and -ve on the chart
@@ -73,51 +108,14 @@ export const Chart: FC<ChartProps> = ({ data = [], isLoading, width, height }) =
   const minValue = Math.min(...dataValues);
   const maxValue = Math.max(...dataValues);
   const offsetY = minValue < 0 && maxValue > 0 ? maxValue - minValue : 0;
-  
+
+  const EmptyOrLoading = isLoading ? LoadingIndicator : Empty;
+  const ChartOrEmpty = data.length ? FullChart : EmptyOrLoading;
+  // const MaybeLoadingIndicator = isLoading ? LoadingIndicator : ChartOrEmpty;
+
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      {!isLoading ? (
-        data.length ? (
-          <VictoryChart
-            width={width}
-            height={height}
-            padding={{ top: 10, bottom: 30, left: 30, right: 30 }}
-            domain={{ y: [domainMin, domainMax], x: [minTime, maxTime] }}
-          >
-            {/* X AXIS */}
-            <VictoryAxis
-              orientation="bottom"
-              offsetY={offsetY}
-              style={style.xAxis}
-              tickFormat={tickFormatter}
-              tickCount={5}
-            />
-
-            {/* Y AXIS */}
-            <VictoryAxis dependentAxis orientation="left" tickCount={5} style={style.yAxis} />
-
-            <ChartGradient />
-            <VictoryLine
-              data={data}
-              x={CHART.X_KEY}
-              y={CHART.Y_KEY}
-              interpolation={CHART.INTERPOLATION}
-              style={style.line}
-            />
-          </VictoryChart>
-        ) : (
-          Empty
-        )
-      ) : (
-        <Centered style={{ width: '100%', minHeight: height }}>
-          <ActivityIndicator size="large" color={COLOUR.PRIMARY} />
-        </Centered>
-      )}
+    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <ChartOrEmpty />
     </View>
   );
 };
